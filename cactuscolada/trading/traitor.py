b6d692b400b833aa91bb48d0b74f3387315ac630
David <davidxiang101@gmail.com>
2024-04-04 21:05:53 -0400

    outline

---
+++ cactuscolada/trading/traitor.py
@@ -0,0 +1,144 @@
+from datamodel import *
+from typing import List
+import json
+from typing import Any
+import numpy as np
+
+class Trader:
+    def __init__(self):
+        resource_data: Dict[Symbol: Data] = {}
+        resource_strategies: Dict[Symbol: Strategy] = {}
+        orderManager: OrderManager = OrderManager()
+
+    def run(self, state: TradingState):
+        result = {}
+        for product in state.order_depths:
+            order_depth: OrderDepth = state.order_depths[product]
+            orders: List[Order] = []
+
+            if len(order_depth.sell_orders) > 0 and len(order_depth.buy_orders) > 0:
+                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                
+                pos = state.position.get(product, 0)
+                buy_price, buy_quantity, sell_price, sell_quantity = self.get_buy_sell(product, pos, best_bid, best_ask)
+                
+                orders.append(Order(product, buy_price, buy_quantity))
+                orders.append(Order(product, sell_price, -sell_quantity))
+            
+            result[product] = orders
+    
+        traderData = "SAMPLE" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
+        
+        conversions = 1
+        logger.flush(state, result, conversions, traderData)
+        return result, conversions, traderData
+
+class Data:
+    def __init__(self) -> None:
+        self.product_limit = 0
+        self.position = 0
+
+class OrderManager:
+    def __init__(self):
+        self.all_orders: Dict[Symbol: list[Order]] = {}
+        pass
+    
+    def createOrder(self, product: Symbol, order: Order):
+        self.all_orders[product].append(order)
+    
+    def getAllOrders(self):
+        return self.all_orders
+    
+class Strategy:
+    def __init__(self, order_manager: OrderManager):
+        pass
+
+    def trade(data: Data):
+        pass
+
+
+
+class Logger:
+    def __init__(self) -> None:
+        self.logs = ""
+
+    def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
+        self.logs += sep.join(map(str, objects)) + end
+
+    def flush(self, state: TradingState, orders: dict[Symbol, list[Order]], conversions: int, trader_data: str) -> None:
+        print(json.dumps([
+            self.compress_state(state),
+            self.compress_orders(orders),
+            conversions,
+            trader_data,
+            self.logs,
+        ], cls=ProsperityEncoder, separators=(",", ":")))
+
+        self.logs = ""
+
+    def compress_state(self, state: TradingState) -> list[Any]:
+        return [
+            state.timestamp,
+            state.traderData,
+            self.compress_listings(state.listings),
+            self.compress_order_depths(state.order_depths),
+            self.compress_trades(state.own_trades),
+            self.compress_trades(state.market_trades),
+            state.position,
+            self.compress_observations(state.observations),
+        ]
+
+    def compress_listings(self, listings: dict[Symbol, Listing]) -> list[list[Any]]:
+        compressed = []
+        for listing in listings.values():
+            compressed.append([listing["symbol"], listing["product"], listing["denomination"]])
+
+        return compressed
+
+    def compress_order_depths(self, order_depths: dict[Symbol, OrderDepth]) -> dict[Symbol, list[Any]]:
+        compressed = {}
+        for symbol, order_depth in order_depths.items():
+            compressed[symbol] = [order_depth.buy_orders, order_depth.sell_orders]
+
+        return compressed
+
+    def compress_trades(self, trades: dict[Symbol, list[Trade]]) -> list[list[Any]]:
+        compressed = []
+        for arr in trades.values():
+            for trade in arr:
+                compressed.append([
+                    trade.symbol,
+                    trade.price,
+                    trade.quantity,
+                    trade.buyer,
+                    trade.seller,
+                    trade.timestamp,
+                ])
+
+        return compressed
+
+    def compress_observations(self, observations: Observation) -> list[Any]:
+        conversion_observations = {}
+        for product, observation in observations.conversionObservations.items():
+            conversion_observations[product] = [
+                observation.bidPrice,
+                observation.askPrice,
+                observation.transportFees,
+                observation.exportTariff,
+                observation.importTariff,
+                observation.sunlight,
+                observation.humidity,
+            ]
+
+        return [observations.plainValueObservations, conversion_observations]
+
+    def compress_orders(self, orders: dict[Symbol, list[Order]]) -> list[list[Any]]:
+        compressed = []
+        for arr in orders.values():
+            for order in arr:
+                compressed.append([order.symbol, order.price, order.quantity])
+
+        return compressed
+
+logger = Logger()
